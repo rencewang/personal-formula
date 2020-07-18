cb83133247cb5dec0e2faf7e1c44a0fb91cf2d6e
law-wang <47406212+law-wang@users.noreply.github.com>
2020-07-18 12:52:56 -0700

    footer dynamic text

--- src/components/aboutblock.js
+++ src/components/aboutblock.js
@@ -1,15 +1,14 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
-// import "../styles/about.scss"
+import style from "../styles/about.scss"
 
 const BlogFeatured = () => {
 
     return (
-        <main class="blogfeatured">
-
-
-
+        <main className={style.about}>
+            <p>I'm a student at &#x2609; Yale studying the intersection between computer science and design and <i>also economics</i>. I like basic things such as watching Chinese TV, making Spotify playlists, crying to YouTube videos, collecting song lyrics, and yelping.
+            If you'd like to find out more about the interesting personality I never knew I had, you can check out my basic <a href="/">opinions</a>, my basic <a href="/">art</a>, or go find me &#x2193; while I get lost in space.</p>
         </main>
     )
 }
--- src/components/blogfeatured.js
+++ src/components/blogfeatured.js
@@ -6,25 +6,25 @@ import "../styles/blogfeatured.scss"
 const BlogFeatured = () => {
 
     return (
-        <main class="blogfeatured">
+        <main className="blogfeatured">
 
-            <div class="accordionpiece">
+            <div className="accordionpiece">
                 <input type="checkbox" id="1" name="accordion" />
-                <label for="1">Norman Fucking Rockwell!: Coexisting Hope and Despair</label>
+                <label htmlFor="1">Norman Fucking Rockwell!: Coexisting Hope and Despair</label>
                 <div>
                     <p>For me, a good Lana song does two things: 1) makes me fall asleep and 2) gives me edgy inspiration upon further examination (the next morning, as it usually turns out). So it should come as no surprise that The Next Best American Record happened to be the first song...</p>
-                    <div class="readmore-links">
+                    <div className="readmore-links">
                         <a href="/" >CONTINUE READING</a>
                     </div>
                 </div>
             </div>
 
-            <div class="accordionpiece">
+            <div className="accordionpiece">
                 <input type="checkbox" id="2" name="accordion" />
-                <label for="2">Norman Fucking Rockwell!: Coexisting Hope and Despair</label>
+                <label htmlFor="2">Norman Fucking Rockwell!: Coexisting Hope and Despair</label>
                 <div>
                     <p>For me, a good Lana song does two things: 1) makes me fall asleep and 2) gives me edgy inspiration upon further examination (the next morning, as it usually turns out). So it should come as no surprise that The Next Best American Record happened to be the first song...</p>
-                    <div class="readmore-links">
+                    <div className="readmore-links">
                         <a href="/" >CONTINUE READING</a>
                     </div>
                 </div>
