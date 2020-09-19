fb0c7996de6a02b71fa8079822b1f64bd321e6d1
law-wang <47406212+law-wang@users.noreply.github.com>
2020-09-19 13:22:30 -0700

    greeting update

--- src/components/tracklist.js
+++ src/components/tracklist.js
@@ -3,6 +3,15 @@ import "../styles/index.scss"
 
 const Tracklist = () => {
 
+    const today = new Date()
+    const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    let day
+    if (today.getDate() < 10) { 
+        day = "0"+today.getDate()
+    } else {
+        day = today.getDate()
+    } 
+
     const request = require('request')
     const client_id = 'fe75f206e45d40dc81c0e21dfba40054'
     const client_secret = '21068bd3ec2e48d2bc4fa04fdd8d7fba'
@@ -34,12 +43,12 @@ const Tracklist = () => {
                 })
             }
         })
-    }, [])    
+    })    
 
     return (
         <div id="playlist-container">
             <div className="load-track">
-                Hello.
+                :) Today is {months[today.getMonth()]}/{day} , {today.getFullYear()}. It's nice to meet you.
             </div>
             {tracks.slice(0, 10).map((item, index) => (
                 <div className="track" key={index}>
