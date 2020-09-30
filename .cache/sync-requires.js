d371f003881c6564b0f603d9e83041b435e8bd64
law-wang <47406212+law-wang@users.noreply.github.com>
2020-09-30 09:04:18 -0700

    redirect breathe

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,6 +7,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---src-pages-404-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/pages/404.js"))),
   "component---src-pages-blog-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/pages/blog.js"))),
+  "component---src-pages-breathe-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/pages/breathe.js"))),
   "component---src-pages-index-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/pages/index.js"))),
   "component---src-pages-project-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/pages/project.js"))),
   "component---src-templates-art-js": (preferDefault(require("/Volumes/Data/Git/formula-update/src/templates/art.js"))),
---
+++ src/pages/breathe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+    
+class Puppies extends Component {
+    componentDidMount() {
+        window.location.replace("https://chrome.google.com/webstore/detail/breathe-minimalist-new-ta/ngcbnnpkcfomilobdpffbkcabmehjnbm");
+    }
+
+    render() {
+        return <div />
+    }
+}
+
+export default Puppies
