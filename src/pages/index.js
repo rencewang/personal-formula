5690d7ca4551b1edae9e6ad81c3b186f4f505c2b
Lawrence Wang <47406212+law-wang@users.noreply.github.com>
2021-03-10 12:00:59 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,21 @@ import "../styles/index.scss"
 
 const IndexPage = () => (
     <Layout>
-      <SEO title="Home" />
-      <div className="index-roll">
-        <Tracklist />
-        <div className="index-featured-group">
-          <div className="index-titles"><span>Projects</span></div>
-          <ProjectFeatured />
+        <SEO title="Home" />
+        <div className="content">
+          <div className="index-roll">
+            <Tracklist />
+            <div className="index-featured-group">
+              <div className="index-titles"><span>Projects</span></div>
+              <ProjectFeatured />
+            </div>
+            <div className="index-featured-group">
+              <div className="index-titles"><span>Writing</span></div>
+              <BlogFeatured />
+            </div>
+            <div className="index-glow"><span className="circle"></span></div>
+          </div>
         </div>
-        <div className="index-featured-group">
-          <div className="index-titles"><span>Writing</span></div>
-          <BlogFeatured />
-        </div>
-        <div className="index-glow"><span className="circle"></span></div>
-      </div>
     </Layout>
 )
 
